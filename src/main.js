5d5a96e128a8fb4b17e2ffcb956be5dcbf4895b3
lvmartinez16 <lizethm745@gmail.com>
2023-08-15 11:27:57 -0500

    dineñoCardImg

--- src/main.js
+++ src/main.js
@@ -11,15 +11,19 @@ const pokemonListDiv = document.getElementById('listPokemon');
 
 // Usa el método map() para crear un div para cada objeto de Pokémon
 const pokemonDivs = data.pokemon.map(pokemon => {
-    console.log(pokemon);
+    //console.log(pokemon);
+    let divType =``;
+    pokemon.type.map(type => {
+        divType += `<a class="type-${type}">${type}</a>`;
+    });
     const pokemonDiv = document.createElement('div');
     pokemonDiv.className = 'tarjetaPokemon';
-    pokemonDiv.innerHTML = `<a class="namePokemon">${pokemon.name}</a> <div class="imgpokemon"><img src="${pokemon.img}" alt=""></div><div `;
+    pokemonDiv.innerHTML = `<div class="card-front"><a class="namePokemon">${pokemon.name}</a><div class="card-type">${divType}</div><div class="imgpokemon"><img src="${pokemon.img}" alt=""></div><div><div class="card-back"></div>`;
     return pokemonDiv;
 });
 
 // Agrega cada div de Pokémon al div principal en el DOM
-console.log("divs ->",pokemonDivs);
+console.log("divs ->", pokemonDivs);
 pokemonDivs.forEach(pokemonDiv => {
     pokemonListDiv.appendChild(pokemonDiv);
 });
