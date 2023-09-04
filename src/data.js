ba4439dfcefbc3b747ac9eb0890198e5fe8fe720
lvmartinez16 <lizethm745@gmail.com>
2023-09-04 17:17:08 -0500

    cargaInicialMain

--- src/data.js
+++ src/data.js
@@ -1,37 +1,7 @@
 import data from './data/pokemon/pokemon.js';
 
-
 export const cargarTipo = (data, comboFiltro) => {
   // let comboFiltro = document.getElementById('numberSelector', valueNumber);
   const typeFilter = data.pokemon.filter(type => type.type.includes(comboFiltro));
   return typeFilter;
 };
-
-
-export const cargaInicial = (dataComplete, pokemonListDiv) => {
-  // Usa el método map() para crear un div para cada objeto de Pokémon
-  //se crea una const pokemonDivs=se utiliza metodo map en la propiedad 'pokemon' del obj. dataComplete
-  //const uniqueTypes = {};
-  const pokemonDivs = dataComplete.pokemon.map(pokemon => {
-
-    let divType = ``;
-    pokemon.type.map(type => {
-      //  uniqueTypes[type] = true;
-      divType += `<a class="type-${type}">${type}</a>`;
-    });
-    const pokemonDiv = document.createElement('div');
-    pokemonDiv.className = 'tarjetaPokemon';
-    pokemonDiv.innerHTML =
-      `<div class="card-front"><a class="namePokemon">${pokemon.name}</a><div class="card-type">${divType}</div><div class="imgpokemon"><img src="${pokemon.img}" alt=""></div><div class="card-back"><div class="card-description"><p>${pokemon.about}</p></div></div></div>`;
-    return pokemonDiv;
-
-  });
-
-
-  //console.log("tipos-->", uniqueTypes);
-  pokemonListDiv.innerHTML = '';
-  // Agrega cada div de Pokémon al div principal en el DOM
-  pokemonDivs.forEach(pokemonDiv => {
-    pokemonListDiv.appendChild(pokemonDiv);
-  });
-}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,69 @@
-import {  cargaInicial,cargarTipo } from './data.js';
+import { cargarTipo } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-console.log("ARRAY",data);
-//se crea una const llamada pokemkonListDiv obteniendo el elemento id 
-//del documento llamado 'listPokemon' que esta en el html
+//se obtiene elemn ID listPokemon' que esta en el html
 const listPokemon = document.getElementById('listPokemon');
-console.log("crea el div donde van las tarjetas", listPokemon);
-//cargaInicial(data);
+cargaInicial(data, listPokemon);
 
+
+function cargaInicial(dataComplete, pokemonListDiv) {
+  const pokemonDivs = dataComplete.pokemon.map(pokemon => {
+    let divType = ``;
+    pokemon.type.forEach(type => {
+      divType += `<a class="type-${type}">${type}</a>`;
+    });
+    const pokemonDiv = document.createElement('div');
+    pokemonDiv.className = 'tarjetaPokemon';
+    pokemonDiv.innerHTML =
+      `<div class="card-front"><a class="namePokemon">${pokemon.name}</a>
+        <div class="card-type">${divType}</div>
+        <div class="imgpokemon"><img src="${pokemon.img}" alt=""></div>
+        <div><div class="card-back">
+        <div class="card-description">
+        <p>${pokemon.about}</p></div>
+        </div>`;
+    return pokemonDiv;
+  });
+
+  pokemonListDiv.innerHTML = '';
+  pokemonDivs.forEach(pokemonDiv => {
+    pokemonListDiv.appendChild(pokemonDiv);
+  });
+}
+
+window.addEventListener('change', changeSelect);
+function changeSelect() {
+  const valueFilter = document.getElementById('typeSelector').value;
+  //  console.log("value---->>>>", valueFilter);
+  if (valueFilter !== 'all') {
+    const pokemonFilter = cargarTipo(data, valueFilter);
+    listPokemon.innerHTML = '';
+    pokemonFilter.forEach(pokemon => {
+      let divType = '';
+      pokemon.type.forEach(type => {
+        divType += `<a class="type-${type}">${type}</a>`;
+      });
+      const pokemonDiv = document.createElement('div');
+      pokemonDiv.className = 'tarjetaPokemon';
+      pokemonDiv.innerHTML =
+        `<div class="card-front"><a class="namePokemon">${pokemon.name}</a>
+        <div class="card-type">${divType}</div>
+        <div class="imgpokemon"><img src="${pokemon.img}" alt=""></div>
+        <div><div class="card-back">
+        <div class="card-description">
+        <p>${pokemon.about}</p></div>
+        </div>`;
+      listPokemon.appendChild(pokemonDiv);
+    });
+  } else {
+    cargaInicial(data, listPokemon);
+  }
+}
+cargaInicial(data, listPokemon); // Llama a cargaInicial al cargar la página 
+
+
+
+/* BOTON SUBIR */
 const scrollToTopButton = document.getElementById("btnVolverArriba");
 //este evento scrolltopbutton baje la pagina, muestra el btn
 window.onscroll = function () {
@@ -23,39 +79,3 @@ scrollToTopButton.addEventListener("click", function () { // Hacer scroll suave 
   //document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
-
-
-window.addEventListener('change', changeSelect);
-
-function changeSelect() {
-  const valueFilter = document.getElementById('typeSelector');
-  console.log("value---->>>>", valueFilter.value);
-  if (valueFilter.value !== 'all') {
-    const pokemonFilter = cargarTipo(data, valueFilter.value);
-    listPokemon.innerHTML = ''; //borra todo el contenido dentro del elemento DIV-lisPokemon
-    pokemonFilter.forEach(pokemon => { // recorre cada elemento en el arreglo pokemons.
-      let divType = ''; //Dec.variable divTypedivType, que se utilizará para construir una cadena de elementos HTML que representan los tipos del Pokémon. 
-      pokemon.type.forEach(type => {//itera a través de cada tipo del Pokémon 
-        divType += `<a class="type-${type}">${type}</a>`;
-      }); ///Para cada tipo, se construye un elemento <a> (enlace) con una clase CSS basada en el tipo del Pokémon,
-      const pokemonDiv = document.createElement('div');
-      pokemonDiv.className = 'tarjetaPokemon'; //asigna la clase CSS 'tarjetaPokemon'
-      //pokemonDiv contenido HTML del div utilizando plantillas de cadena crearestructura de una tarjeta Pokémon
-      //contiene toda la información de la tarjeta Pokémon 
-      pokemonDiv.innerHTML =
-        `<div class="card-front"><a class="namePokemon">${pokemon.name}</a>
-    <div class="card-type">${divType}</div>
-    <div class="imgpokemon"><img src="${pokemon.img}" alt=""></div>
-    <div><div class="card-back">
-    <div class="card-description">
-    <p>${pokemon.about}</p></div>
-    </div>`;
-      /* 
-  se agrega como hijo al elemento con el ID pokemonListDiv, inserta en la página web. */
-      listPokemon.appendChild(pokemonDiv);
-
-    });
-  } else { // es igual a all ,se ejecuta la funcion inicial que no tiene filtros
-    cargaInicial(data, listPokemon);
-  }
-} 
